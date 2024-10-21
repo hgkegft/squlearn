78e5b579b89f3ce3d49c44ceedf0f9ad161d12aa
Henning Krause <henning.krause@gft.com>
2024-10-21 10:28:31 +0200

    Fix kernel error.

--- src/squlearn/kernel/matrix/projected_quantum_kernel.py
+++ src/squlearn/kernel/matrix/projected_quantum_kernel.py
@@ -157,7 +157,7 @@ class OuterKernelBase:
                 """
                 self._kernel.set_params(**params)
 
-        return SklearnOuterKernel(kernel, **kwarg)
+        return SklearnOuterKernel(kernel=kernel, **kwarg)
 
 
 class ProjectedQuantumKernel(KernelMatrixBase):
